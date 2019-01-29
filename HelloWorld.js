a87535270928ae5fbdb10ae3730b431907cdbbaf
Anthony Ortega <aortega1285@mail.fresnostate.edu>
2019-01-28 18:17:24 -0800

    added middle name

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,13 +1,15 @@
 var HelloWorld = (function () {
-    function HelloWorld(firstName, lastName) {
+    function HelloWorld(firstName, middleName, lastName) {
         this.firstName = '';
         this.firstName = firstName;
+        this.middleName = middleName;
         this.lastName = lastName;
     }
     HelloWorld.prototype.sayHello = function () {
-        console.log('Hello World!' + ' My name is ' + this.firstName + ' ' + this.lastName + ".");
+        console.log('Hello World!' + ' My name is ' + this.firstName + ' ' + this.middleName + ' ' + this.lastName + ".");
     };
     return HelloWorld;
 })();
-var myHelloClassInstance = new HelloWorld('Anthony', 'Ortega');
+var myHelloClassInstance = new HelloWorld('Anthony', 'Marcus', 'Ortega');
 myHelloClassInstance.sayHello();
+//# sourceMappingURL=HelloWorld.js.map
--- helloworld.ts
+++ helloworld.ts
@@ -1,18 +1,20 @@
 class HelloWorld{
     
     firstName = '';
+    middleName: string;
     lastName: string;
   
-    constructor(firstName: string, lastName: string){
+    constructor(firstName: string, middleName: string, lastName: string){
         this.firstName = firstName;
+        this.middleName = middleName
         this.lastName = lastName;
     }
 
     sayHello() {
-        console.log('Hello World!' + ' My name is ' + this.firstName + ' ' + this.lastName + ".");
+        console.log('Hello World!' + ' My name is ' + this.firstName + ' ' + this. middleName + ' ' + this.lastName + ".");
     }
 
 }
 
-const myHelloClassInstance = new HelloWorld('Anthony', 'Ortega');
+const myHelloClassInstance = new HelloWorld('Anthony', 'Marcus', 'Ortega');
 myHelloClassInstance.sayHello();
